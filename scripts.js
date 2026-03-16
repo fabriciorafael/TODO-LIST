abc7e61fcb787ad6efc063534cb72fe5e682dd43
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-03-16 15:58:25 -0300

    Projeto finalizado - To Do List

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
 /*
 Lista de tarefas
-[] Saber qaundo o botao foi clicado
-[]Pegar o texto dentro do input
-[]Colocar o texto na tela
+[x] Saber qaundo o botao foi clicado
+[x]Pegar o texto dentro do input
+[x]Colocar o texto na tela
+[x] Deletar a tatefa da tela (Quando eu clicar no X)
 */
 
 function adicionarTarefa() {
-    console.log("Clique no botao")
+    let valorDoInput = document.querySelector('input').value
+
+    let li = document.createElement("li")
+    li.innerHTML = valorDoInput + '<span onclick="deletarTarefa(this)">❌</span>'
+
+    document.querySelector("ul").appendChild(li)
+
+    document.querySelector('input').value = ''
+
 }
 
-console.log("Executa direto")
+function deletarTarefa(li) {
+    li.parentElement.remove()
+
+
+}
+
